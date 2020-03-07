91deb3dd22594dea411fe04a68941f36d10f6245
Alisson Morais <alissonfpmorais@olhonocarro.com.br>
2020-03-07 16:51:41 -0300

    :wrench: update infrastructure setup

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :b2ml, B2ml.Repo,
   username: "postgres",
   password: "postgres",
   database: "b2ml_dev",
-  hostname: "localhost",
+  hostname: "db",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
