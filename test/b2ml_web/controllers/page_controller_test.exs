af45aafebfea88299717f7b8524c431b88deef6c
Alisson Morais <alissonfpmorais@olhonocarro.com.br>
2020-03-09 08:45:24 -0300

    :white_check_mark: update page controller tests

--- test/b2ml_web/controllers/page_controller_test.exs
+++ test/b2ml_web/controllers/page_controller_test.exs
@@ -1,8 +1,9 @@
 defmodule B2mlWeb.PageControllerTest do
   use B2mlWeb.ConnCase
+  import B2mlWeb.Gettext
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ gettext("Welcome to %{name}!", %{name: "B2ml School"})
   end
 end
